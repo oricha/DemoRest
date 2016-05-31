0d2b732a85be8ec3be6cd363d872d7cdf1f46191
kmuniz <kmuniz@KMUNIZ-IBM.stacks-cis.ct>
2016-05-31 14:51:59 +0200

    Added Tests

--- src/test/java/org/example/kmuniz/DemoRestApplicationTests.java
+++ src/test/java/org/example/kmuniz/DemoRestApplicationTests.java
@@ -1,18 +1,44 @@
 package org.example.kmuniz;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = DemoRestApplication.class)
 @WebAppConfiguration
 public class DemoRestApplicationTests {
 
-	@Test
-	public void contextLoads() {
+	@Autowired
+	private WebApplicationContext webApplicationContext;
+
+	private MockMvc mockMvc;
+
+	@Before
+	public void before() throws Exception {
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
 	}
 
+	@Test
+	public void getTransactionsTest() throws Exception {
+
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/transactions/"))
+				.andExpect(MockMvcResultMatchers.status().isOk());
+	}
+
+	@Test
+	public void getTransactionTest() throws Exception {
+
+		this.mockMvc.perform(MockMvcRequestBuilders.get("/transactions/123"))
+				.andExpect(MockMvcResultMatchers.status().isOk());
+	}
 }
